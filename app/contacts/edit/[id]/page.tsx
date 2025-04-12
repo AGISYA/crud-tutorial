b57f67a815f354cc44254a2e34197ac896c2bda5
AGISYA <abdiagisya@gmail.com>
2025-04-12 09:21:33 +0700

    fix elor 3

--- app/contacts/edit/[id]/page.tsx
+++ app/contacts/edit/[id]/page.tsx
@@ -2,13 +2,18 @@ import UpdateForm from "@/components/edit-form";
 import { getContactById } from "@/lib/data";
 import { notFound } from "next/navigation";
 
-const UpdateContactPage = async ({ params }: { params: { id: string } }) => {
+interface PageProps {
+  params: { id: string };
+}
+
+const UpdateContactPage = async ({ params }: PageProps) => {
   const id = params.id;
   const contact = await getContactById(id);
 
   if (!contact) {
     notFound();
   }
+
   return (
     <div className="max-w-md mx-auto mt-5">
       <h1 className="text-2xl text-center mb-2">Update Contact</h1>
