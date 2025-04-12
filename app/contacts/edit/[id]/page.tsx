16f1c5081a7cd593abf188c150e56948baa2858e
AGISYA <abdiagisya@gmail.com>
2025-04-12 09:25:51 +0700

    fix elor 5

--- app/contacts/edit/[id]/page.tsx
+++ app/contacts/edit/[id]/page.tsx
@@ -2,18 +2,20 @@ import UpdateForm from "@/components/edit-form";
 import { getContactById } from "@/lib/data";
 import { notFound } from "next/navigation";
 
+// Pastikan fungsi handler untuk halaman ini async
 const UpdateContactPage = async ({ params }: { params: { id: string } }) => {
-  const id = params.id;
-  const contact = await getContactById(id);
+  const { id } = params; // Mengambil id dari params
+  const contact = await getContactById(id); // Mengambil data kontak berdasarkan id
 
   if (!contact) {
-    notFound();
+    notFound(); // Jika kontak tidak ditemukan, tampilkan halaman 404
   }
 
   return (
     <div className="max-w-md mx-auto mt-5">
       <h1 className="text-2xl text-center mb-2">Update Contact</h1>
-      <UpdateForm contact={contact} />
+      <UpdateForm contact={contact} />{" "}
+      {/* Render form untuk mengupdate kontak */}
     </div>
   );
 };
